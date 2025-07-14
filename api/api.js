e2942cb91fcd3554dfaa87da06dce8140fc49aae
Aryaveer Agrawal <krish140705@gmail.com>
2025-07-14 14:01:00 +0530

    change

--- api/api.js
+++ api/api.js
@@ -2,9 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-        ? 'http://127.0.0.1:8102/'
+        ? 'http://192.168.29.210:8102/'  // Your actual network IP
         : '/api/'
 });
-
 // Use it right away or export it
 export default api;
